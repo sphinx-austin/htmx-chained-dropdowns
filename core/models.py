8a9e2f0ce61387419e343b4401028dbba4b8f2aa
sphinx-austin <austinjuniour4@gmail.com>
2023-03-24 05:24:15 +0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    name =  models.CharField(max_length=128)
+
+class Module(models.Model):
+    name =  models.CharField(max_length=128)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+#url patterns
+urlpatterns = [
+    path('', views.courses, name='courses'),
+    path('modules/', views.modules, name='modules'),
+]
